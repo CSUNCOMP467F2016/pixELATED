add0c1d1fa790fe1ee6993d2d78b88000d2b6622
jeoneil <jack.oneil.508@my.csun.edu>
2016-11-21 15:06:28 -0800

    Color Parameters Update #1
    Enhanced UI and improved compatibility with other tools.
    (+) Labels and message texts added
    (+) “Commit” and “Reset” functions added
    (+) Contrast now functions as expected
    (+) Sequential altercations to color parameters are correctly applied on
    the original image
    (+) Changes now save to the canvas (with “Commit”) and will appear when
    other tools are implemented
    
    Bugs
    (-) Re-selecting color parameters tools (after the tool has been
    “destroyed”) will return the canvas to a previous state, omitting any
    changes to it since. [This is a problem with the Caman library.]
    (-) When you “Reset”, the sliders won’t reset their positions [There is
    no easy fix to this.]
    
    Let me know if you find any more bugs.

--- scripts/js/Tools/BriAndConTool.js
+++ scripts/js/Tools/BriAndConTool.js
@@ -1,71 +1,157 @@
 define(  [ 'jquery', 'd3', 'Canvas', 'Caman' ],
 function (     $   ,  d3 ,  Canvas ,  Caman  ) {
-
+  var caman;
   var BriAndConTool = {
-    brightness: 0,
-    contrast: 0,
-    make: function() {
-      this.setupOptions();
+	make: function() {
+    	var con = 0, bri = 0;
+    	var options, src; 
+    	var displayCon, displayBri, sliderCon, sliderBri;
+    	var makeSlider;
+        var clickCommit, clickReset, slideChangeCon, slideChangeBri;
+        var messageText, sendMessage;
+        var updateImage;
+        
+        initPanel= function() {
+        	// Contrast label
+            options.append('text')
+            	.text('Contrast')
+            	.style('position','absolute')
+            	.style('left', '0px')
+            	.style('top', '0px');
+            			
+            // Contrast slider
+            sliderCon = options.append( 'input' )
+    		    .attr( 'type', 'range' )
+    		    .attr( 'min', '-100' )
+    		    .attr( 'max', '100' )
+    		    .attr( 'value', con )
+    		    .attr( 'step', '1' )
+    		    .style('position','absolute')
+    		    .style('left', '85px')
+    	  	    .style('top', '0px')
+    		    .on( 'change', slideChangeCon);
+            
+            // Contrast value display
+            displayCon = options.append('text')
+            	.attr('id' ,'conValue')
+            	.text(con)
+    	        .style('position','absolute')
+    	    	.style('left', '225px')
+    	    	.style('top', '0px');
+            
+            // Brightness label
+            options.append('text')
+    	      	.text('Brightness')
+    	        .style('position','absolute')
+    	    	.style('left', '0px')
+    	    	.style('top', '50px');
+            
+            // Brightness slider
+            sliderBri = options.append( 'input' )
+    	        .attr( 'id', 'brightnessSlider' )
+    	        .attr( 'type', 'range' )
+    	        .attr( 'min', '-100' )
+    	        .attr( 'max', '100' )
+    	        .attr( 'value', con )
+    	        .attr( 'step', '1' )
+    	        .style('position','absolute')
+    	        .style('left', '85px')
+    	        .style('top', '50px')
+    	        .on( 'change', slideChangeBri);
+            
+            // Brightness value display
+            displayBri = options.append('text')
+    	      	.attr('id' ,'briValue')
+    	      	.text(bri)
+    	        .style('position','absolute')
+    	    	.style('left', '225px')
+    	    	.style('top', '50px');
+          		
+            // Commit button
+            options.append('button')
+            	.text('Commit')
+            	.attr('type', 'button')
+            	.style('width', '75px')
+            	.style('height', '25px')
+    	        .style('position','absolute')
+    	    	.style('left', '50px')
+    	    	.style('top', '100px')
+            	.on('click', clickCommit);
+            
+            // Reset button
+            options.append('button')
+    	    	.text('Reset')
+    	    	.attr('type', 'button')
+    	    	.style('width', '75px')
+    	    	.style('height', '25px')
+    	        .style('position','absolute')
+    	    	.style('left', '125px')
+    	    	.style('top', '100px')
+    	    	.on('click', clickReset);
+            
+            //add update message textbox
+            messageText = options.append('text')
+            	.style('position','absolute')
+		    	.style('left', '0px')
+		    	.style('top', '150px')
+		    	.style('width', '200px');
+        };
+        
+        sendMessage = function(message) { 
+        	messageText.text(message);
+        	// message will fade away after 5 seconds
+        	setTimeout(function() {
+        		messageText.text('');
+        	}, 5000);
+        }
+        clickCommit = function() {
+        	src = caman.toBase64();
+        	Canvas.setImage(src);
+        	console.log('Inputted color parameters committed to canvas.');
+        	sendMessage('Changes saved to canvas.');
+        };
+        clickReset = function() {
+        	con = 0;
+        	bri = 0;
+        	caman.reset();
+        	sliderCon.value = con; 
+        	sliderCon.value = bri;
+        	
+        	displayBri.text(bri);
+        	displayCon.text(con);
+        	console.log('Visual canvas reset.');
+        	sendMessage('Visual canvas reset.');
+        };
+        slideChangeCon = function() {
+        	con = this.value;
+        	updateImage(); 
+        	caman.render();
+        	console.log('Contrast changed to ' + con + ' .');
+            displayCon.text(con);
+        };
+        slideChangeBri = function() {
+        	bri = this.value;
+            updateImage(); 
+            caman.render();
+            console.log('Brightness changed to ' + bri + ' .');
+            displayBri.text(bri);
+        };
+        updateImage = function() {
+        	caman.reset();
+        	// Faman's contrast won't work if argument isn't parsed as int
+    		caman.contrast(parseInt(con));
+    		console.log(con);
+    		caman.brightness(bri);
+        };
+        
+        caman = Caman(Canvas.visual.canvas, Canvas.canvas.toDataURL());
+        options = d3.select( '#options' );
+        initPanel();
     },
+    
     destroy: function() {
-      //Called when the user activates another tool
-      Caman( Canvas.canvas, function () {
-        // manipulate image here
-        // Arguments: (contrast)
-        this.contrast( BriAndConTool.contrast );
-        this.render( function() {
-          Canvas.image.src = Canvas.canvas.toDataURL();
-        } );
-      } );
-    },
-    sliderChange: function( val, s ) {
-
-    	if (s == 'brightness') {
-    		BriAndConTool.brightness = val;
-    	}
-    	if (s == 'contrast') {
-    		BriAndConTool.contrast = val;
-    	}
-    	Caman( Canvas.visual.canvas, function () {
-      	// manipulate image here
-      	// Arguments: (brightness, contrast)
-		if (s == 'brightness') {
-			this.brightness( BriAndConTool.brightness );
-    	}
-    	if (s == 'contrast') {
-    		this.contrast( BriAndConTool.contrast );
-    	}
-        this.render();
-        this.revert(false);
-      } );
-    },
-    setupOptions: function() {
-      var options = d3.select( '#options' );
-      
-      //brightness Slider
-      options.append( 'input' )
-        .attr( 'id', 'BriAndConTool' )
-        .attr( 'type', 'range' )
-        .attr( 'min', '-100' )
-        .attr( 'max', '100' )
-        .attr( 'value', '0' )
-        .attr( 'step', '1' )
-        .on( 'change', function() {
-          var val = +this.value;
-          BriAndConTool.sliderChange( val, 'brightness' );
-        } );
-      // contrast Slider
-      options.append( 'input' )
-      .attr( 'id', 'BriAndConTool' )
-      .attr( 'type', 'range' )
-      .attr( 'min', '-100' )
-      .attr( 'max', '100' )
-      .attr( 'value', '0' )
-      .attr( 'step', '1' )
-      .on( 'change', function() {
-        var val = +this.value;
-        BriAndConTool.sliderChange( val, 'contrast' );
-      } );
+    	caman.replaceCanvas(Canvas.visual.canvas);
+    	Canvas.refresh();
     }
   };
 
--- scripts/js/Tools/SatAndHueTool.js
+++ scripts/js/Tools/SatAndHueTool.js
@@ -1,71 +1,155 @@
 define(  [ 'jquery', 'd3', 'Canvas', 'Caman' ],
 function (     $   ,  d3 ,  Canvas ,  Caman  ) {
-
+  var caman;
   var SatAndHueTool = {
-    hue: 0,
-    saturation: 0,
-    make: function() {
-      this.setupOptions();
+	make: function() {
+    	var hue = 0, sat = 0;
+    	var options, src; 
+    	var displayHue, displaySat, sliderHue, sliderSat;
+    	var makeSlider;
+        var clickCommit, clickReset, slideChangeHue, slideChangeSat;
+        var messageText, sendMessage;
+        var updateImage;
+        
+        initPanel= function() {
+        	// Hue label
+            options.append('text')
+            	.text('Hue')
+            	.style('position','absolute')
+            	.style('left', '0px')
+            	.style('top', '0px');
+            			
+            // Hue slider
+            sliderHue = options.append( 'input' )
+    		    .attr( 'type', 'range' )
+    		    .attr( 'min', '0' )
+    		    .attr( 'max', '100' )
+    		    .attr( 'value', hue )
+    		    .attr( 'step', '1' )
+    		    .style('position','absolute')
+    		    .style('left', '85px')
+    	  	    .style('top', '0px')
+    		    .on( 'change', slideChangeHue);
+            
+            // Hue value display
+            displayHue = options.append('text')
+            	.attr('id' ,'hueValue')
+            	.text(hue)
+    	        .style('position','absolute')
+    	    	.style('left', '225px')
+    	    	.style('top', '0px');
+            
+            // Saturation label
+            options.append('text')
+    	      	.text('Saturation')
+    	        .style('position','absolute')
+    	    	.style('left', '0px')
+    	    	.style('top', '50px');
+            
+            // Saturation slider
+            sliderSat = options.append( 'input' )
+    	        .attr( 'id', 'saturationSlider' )
+    	        .attr( 'type', 'range' )
+    	        .attr( 'min', '-100' )
+    	        .attr( 'max', '100' )
+    	        .attr( 'value', hue )
+    	        .attr( 'step', '1' )
+    	        .style('position','absolute')
+    	        .style('left', '85px')
+    	        .style('top', '50px')
+    	        .on( 'change', slideChangeSat);
+            
+            // Saturation value display
+            displaySat = options.append('text')
+    	      	.attr('id' ,'satValue')
+    	      	.text(sat)
+    	        .style('position','absolute')
+    	    	.style('left', '225px')
+    	    	.style('top', '50px');
+          		
+            // Commit button
+            options.append('button')
+            	.text('Commit')
+            	.attr('type', 'button')
+            	.style('width', '75px')
+            	.style('height', '25px')
+    	        .style('position','absolute')
+    	    	.style('left', '50px')
+    	    	.style('top', '100px')
+            	.on('click', clickCommit);
+            
+            // Reset button
+            options.append('button')
+    	    	.text('Reset')
+    	    	.attr('type', 'button')
+    	    	.style('width', '75px')
+    	    	.style('height', '25px')
+    	        .style('position','absolute')
+    	    	.style('left', '125px')
+    	    	.style('top', '100px')
+    	    	.on('click', clickReset);
+            
+            //add update message textbox
+            messageText = options.append('text')
+            	.style('position','absolute')
+		    	.style('left', '0px')
+		    	.style('top', '150px')
+		    	.style('width', '200px');
+        };
+        
+        sendMessage = function(message) { 
+        	messageText.text(message);
+        	// message will fade away after 5 seconds
+        	setTimeout(function() {
+        		messageText.text('');
+        	}, 5000);
+        }
+        clickCommit = function() {
+        	src = caman.toBase64();
+        	Canvas.setImage(src);
+        	console.log('Inputted color parameters committed to canvas.');
+        	sendMessage('Changes saved to canvas.	');
+        };
+        clickReset = function() {
+        	hue = 0;
+        	sat = 0;
+        	caman.reset();
+        	sliderHue.value = hue; 
+        	sliderHue.value = sat;
+        	
+        	displaySat.text(sat);
+        	displayHue.text(hue);
+        	console.log('Visual canvas reset.');
+        	sendMessage('Visual canvas reset.');
+        };
+        slideChangeHue = function() {
+        	hue = this.value;
+        	updateImage(); 
+        	caman.render();
+        	console.log('Hue changed to ' + hue + ' .');
+            displayHue.text(hue);
+        };
+        slideChangeSat = function() {
+        	sat = this.value;
+            updateImage(); 
+            caman.render();
+            console.log('Saturation changed to ' + sat + ' .');
+            displaySat.text(sat);
+        };
+        updateImage = function() {
+        	caman.reset();
+    		caman.hue(hue);
+    		caman.saturation(sat);
+        };
+        
+        caman = Caman(Canvas.visual.canvas, Canvas.canvas.toDataURL());
+        options = d3.select( '#options' );
+        initPanel();
     },
+    
     destroy: function() {
-      //Called when the user activates another tool
-      Caman( Canvas.canvas, function () {
-        // manipulate image here
-        // Arguments: (saturation)
-        this.saturation( SatAndHueTool.saturation );
-        this.render( function() {
-          Canvas.image.src = Canvas.canvas.toDataURL();
-        } );
-      } );
-    },
-    sliderChange: function( val, s ) {
-
-    	if (s == 'hue') {
-    		SatAndHueTool.hue = val;
-    	}
-    	if (s == 'saturation') {
-    		SatAndHueTool.saturation = val;
-    	}
-    	Caman( Canvas.visual.canvas, function () {
-      	// manipulate image here
-      	// Arguments: (hue, saturation)
-		if (s == 'hue') {
-			this.hue( SatAndHueTool.hue );
-    	}
-    	if (s == 'saturation') {
-    		this.saturation( SatAndHueTool.saturation );
-    	}
-        this.render();
-        this.revert(false);
-      } );
-    },
-    setupOptions: function() {
-      var options = d3.select( '#options' );
-      
-      //Hue Slider
-      options.append( 'input' )
-        .attr( 'id', 'SatAndHueTool' )
-        .attr( 'type', 'range' )
-        .attr( 'min', '0' )
-        .attr( 'max', '100' )
-        .attr( 'value', '0' )
-        .attr( 'step', '1' )
-        .on( 'change', function() {
-          var val = +this.value;
-          SatAndHueTool.sliderChange( val, 'hue' );
-        } );
-      // Saturation Slider
-      options.append( 'input' )
-      .attr( 'id', 'SatAndHueTool' )
-      .attr( 'type', 'range' )
-      .attr( 'min', '-100' )
-      .attr( 'max', '100' )
-      .attr( 'value', '0' )
-      .attr( 'step', '1' )
-      .on( 'change', function() {
-        var val = +this.value;
-        SatAndHueTool.sliderChange( val, 'saturation' );
-      } );
+    	caman.replaceCanvas(Canvas.visual.canvas);
+    	Canvas.refresh();
     }
   };
 
