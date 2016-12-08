d6109b1213f66c6295fe5f045337389f89c65469
Jose Perez <joseluisp92@gmail.com>
2016-12-07 21:39:37 -0800

    Crop tool done

--- scripts/js/Tools/CropTool.js
+++ scripts/js/Tools/CropTool.js
@@ -2,30 +2,25 @@
 define(  [ 'jquery', 'Canvas', 'd3' , 'Cropperjs' ],
     function (     $, Canvas, d3, Cropperjs ) {
         var cropper;
-        var croppedImg;
+        var cropCanvasData;
         var imgURL;
         var CropTool = {
             make: function () {
                 this.setupOptions();
-                var image = document.getElementById('visualcanvas');
+
+                var image = document.getElementById( 'visualcanvas' );
+
                 cropper = new Cropper(image, {
                     aspectRatio: 16 / 9,
                     viewMode: 1,
-                    crop: function (e) {
-                        // console.log( e.detail.x );
-                        // console.log( e.detail.y );
-                        // console.log( e.detail.width );
-                        // console.log( e.detail.height );
-                        // console.log( e.detail.rotate );
-                        // console.log( e.detail.scaleX );
-                        // console.log( e.detail.scaleY );
-                        var cropX = e.detail.x;
-                        // console.log(cropX);
+                    movable: false,
+                    crop: function ( e ) {
                     },
-                    cropend: function (e) {
-                        croppedImg = cropper.getCroppedCanvas(image);
-                        imgURL = croppedImg.toDataURL();
-                        console.log(croppedImg);
+                    cropend: function ( e ) {
+                        cropCanvasData = cropper.getCroppedCanvas();
+                        imgURL = cropCanvasData.toDataURL();
+                        console.log(cropCanvasData);
+                        console.log(imgURL);
 
                     }
                 });
@@ -38,20 +33,18 @@ define(  [ 'jquery', 'Canvas', 'd3' , 'Cropperjs' ],
             //Crop button added to options div in index
             setupOptions: function () {
                 var options = d3.select('#options');
+
                 //cropping button
                 options.append( 'button' )
                     .html( 'Crop' )
-                    .on( 'click', cropBtn);
+                    .on( 'click', cropBtn );
             },
     };
-    //What happens when you click the crop button
+    //Replace image with whats inside crop box
     function cropBtn()
     {
-        // Canvas.context.drawImage();
-        Canvas.redraw();
-        Canvas.image.src = imgURL;
-        console.log("click");
-
+        cropper.replace( imgURL );
+        console.log("Image replaced");
         };
 
         return CropTool;
